b3eb63b6e115b4313bdf7ff7314ba73b2c3254cb
TyAiTi <tuhoangty1@gmail.com>
2018-01-19 15:27:06 +0700

    xin chao

--- hello.c
+++ hello.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 int main(){
-  
-printf("hello, world!Xin chao ban Hai Duyen \nd");
-  printf("Chieu nay toi se sua may cho ban \n");
-return 0;
+printf("Xin chao ban!\n ");
 }
+
