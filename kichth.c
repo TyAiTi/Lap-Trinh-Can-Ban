3d0f20bd1ba96eded83c9e51e48b4044ad89931b
TyAiTi <tuhoangty1@gmail.com>
2018-01-24 17:46:28 +0700

    toi them file kic thuoc  nua do

---
+++ kichth.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+	printf("Size of int is %ld\n",sizeof(int));
+	 printf("Size of long is %ld\n",sizeof(long));
+	 printf("Size of char is %ld\n",sizeof(char));
+	 printf("Size of short is %ld\n",sizeof(short));
+	 printf("Size of foat is %ld\n",sizeof(float));
+	 printf("Size of double is %ld\n",sizeof(double));
+	return 0;
+  
+}
