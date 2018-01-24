bfa1dcc26e52a7c0a3a948ee84022c0b1c25232f
TyAiTi <tuhoangty1@gmail.com>
2018-01-24 17:40:03 +0700

    Toi moi them hang thuong vao do

---
+++ hangthuong.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main () {
+	int r =2;
+	/*Khai bao hang thuong PI bang 3.14*/
+	 const double PI = 3.14;
+	double s;
+	s = PI*r*r;
+	printf("Dien tich vong trong nay la: %.2f\n",s);
+	return 0;
+}
+
